4a6b7fb39e0b5bacc53467504389c7174ee66520
Startseva Tatyana <TatyanaStartseva2020@yandex.ru>
2024-04-06 19:07:32 +0300

    add file

--- Adds_Links_Bot.py
+++ Adds_Links_Bot.py
@@ -6,7 +6,6 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-db_file_path = 'C:/Users/User/PycharmProjects/pythonProject5/Bot/Links/database.db'
 from dotenv import load_dotenv
 
 load_dotenv()
