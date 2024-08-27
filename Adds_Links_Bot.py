54f75c0b5b5236050f565c6049a8dbb07c4a5d31
Startseva Tatyana <TatyanaStartseva2020@yandex.ru>
2024-08-27 15:27:02 +0300

    Замена био default на пустое значение

--- Adds_Links_Bot.py
+++ Adds_Links_Bot.py
@@ -9,7 +9,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from dotenv import load_dotenv
 from openpyxl import Workbook
 import psycopg2
-
+import logging
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -93,35 +93,36 @@ async def download_links(message: types.Message, state: FSMContext):
                                     for chat_id in chat_ids:
                                         cursor.execute("SELECT  user_chat.user_id  FROM user_chat JOIN users ON user_chat.user_id=users.user_id WHERE chat_id = %s  AND bio = 'Default-value-for-parser' GROUP BY user_chat.user_id", (chat_id,))
                                         result = cursor.fetchall()
-                                        if len(result) == 0:
-                                            cursor.execute("SELECT user_id FROM user_chat WHERE chat_id = %s", (chat_id,))
-                                            users = cursor.fetchall()
-                                            for user in users:
-                                                cursor.execute("SELECT * FROM users WHERE user_id = %s", (user,))
-                                                user_data = cursor.fetchall()
-                                                user_id = user_data[0][0]
-                                                if user_id not in user_ids_written :
-                                                    chat_users.append(
-                                                        (
-                                                            user_data[0][0],
-                                                            user_data[0][1],
-                                                            user_data[0][2],
-                                                            user_data[0][3],
-                                                            user_data[0][4],
-                                                            user_data[0][5],
-                                                            user_data[0][6],
-                                                            user_data[0][7],
-                                                            user_data[0][8],
-                                                        )
+                                        cursor.execute("SELECT user_id FROM user_chat WHERE chat_id = %s", (chat_id,))
+                                        users = cursor.fetchall()
+                                        for user in users:
+                                            cursor.execute("SELECT * FROM users WHERE user_id = %s", (user,))
+                                            user_data = cursor.fetchall()
+                                            user_id = user_data[0][0]
+                                            if user_id not in user_ids_written:
+                                                print(user_data)
+                                                chat_users.append(
+                                                    (
+                                                        user_data[0][0],
+                                                        user_data[0][1],
+                                                        user_data[0][2],
+                                                        user_data[0][3],
+                                                        user_data[0][4],
+                                                        user_data[0][5],
+                                                        user_data[0][6],
+                                                        user_data[0][7],
+                                                        user_data[0][8],
                                                     )
-                                                    user_ids_written.add(user_id)
-                                        else:
+                                                )
+                                                user_ids_written.add(user_id)
+                                        if len(result) != 0:
                                             chat_processing+= url+', '
                                 for user in chat_users:
+                                    bio = 'None' if user[2] == 'Default-value-for-parser' else user[2]
                                     user_data = [
                                         user[0],
                                         user[1],
-                                        user[2],
+                                        bio,
                                         user[3],
                                         user[4],
                                         user[5].strftime("%Y-%m-%d %H:%M:%S") if user[5] is not None else "",
